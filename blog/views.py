936b80ef2552caeea3b1fe9f6a2b3f496fa66266
Rafael Ferreira Vasconcelos <rafael.ferreira.8anoa@gmail.com>
2026-07-02 15:49:47 -0300

    Lógica de fazer posts no blog

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
 
+posts = [
+    {
+        'author': 'Rafael Ferreira',
+        'title' : '1º post',
+        'content': 'Hi everbody, my name is Rafael, and i am learning how to use DJANGO!',
+        'date':'July 2, 2026',
+    }, # <--- A VÍRGULA VAI AQUI!
+    {
+        'author': 'Pedro Gustavo',
+        'title' : '2º post',
+        'content': 'I am pretty hyped w/ the 2026 world cup',
+        'date': 'July 10, 2026',
+    }
+]
+
 
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'post':posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
     return render(request,'blog/about.html')
